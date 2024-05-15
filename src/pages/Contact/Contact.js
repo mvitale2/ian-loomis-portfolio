1c7301f07d6eea3fb6415dd853e86594e6fa1527
Skel <mvitale738@gmail.com>
2024-05-14 20:56:18 -0400

    form page styles

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Contact.css';
 
+// Use this tutorial to create email form: https://medium.com/@thomasaugot/create-a-react-contact-form-with-email-js-cad2c8606f33
 function Contact() {
   return (
-    <div>Contact</div>
+    <main className='contact-wrapper'>
+      <div className='left'>
+        <span>For commissions and project inquiries, please email: ian.loomis@email.com or submit a message via this form.</span>
+      </div>
+      <div className='right'>
+        <form>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' />
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' />
+          <label htmlFor='message'>Message</label>
+          <textarea name='message'></textarea>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
