c4470b48276b9d38de93203f0789bc7c71ff4d3f
Skel <mvitale738@gmail.com>
2024-05-16 20:38:35 -0400

    switched back to emailjs

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,61 +1,62 @@
 import React from "react";
-import { useState } from "react";
-// import emailjs from "@emailjs/browser";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { FaArrowRightLong } from "react-icons/fa6";
 import "./Contact.css";
 
-// Used this tutorial to create email form: https://medium.com/@thomasaugot/create-a-react-contact-form-with-email-js-cad2c8606f33
+// Used this tutorial to create initial iteration of email form: https://medium.com/@thomasaugot/create-a-react-contact-form-with-email-js-cad2c8606f33
+// Used this tutorial for netlify form integration: https://www.youtube.com/watch?v=gi-iKLkB8R8
 function Contact() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [stateMessage, setStateMessage] = useState(null);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  console.log(`name: ${name}, email: ${email}, message: ${message}`)
-  // const form = useRef();
+  console.log(`name: ${name}, email: ${email}, message: ${message}`);
+  const form = useRef();
 
-  // const sendEmail = (e) => {
+   const sendEmail = (e) => {
+     e.persist();
+     e.preventDefault();
+     setIsSubmitting(true);
+     // replace service id and update template later
+     // currently these messages will go to my email
+     // integrate netlify with sendForm code
+     emailjs
+       .sendForm("service_ovv4bhg", "template_mp16n74", form.current, {
+         publicKey: "dif7BQrUeE-vta83w",
+       })
+       .then(
+         (result) => {
+           setStateMessage("Message sent!");
+           setIsSubmitting(false);
+           setTimeout(() => {
+             setStateMessage(null);
+           }, 5000); //hides the message after 5 seconds
+         },
+         (error) => {
+           setStateMessage("Something went wrong, please try again later.");
+           setIsSubmitting(false);
+           setTimeout(() => {
+             setStateMessage(null);
+           }, 5000);
+         }
+       );
+     // Clears the form after sending the email
+     e.target.reset();
+   };
+
+  // const handleSubmit = (e) => {
   //   e.persist();
   //   e.preventDefault();
   //   setIsSubmitting(true);
-  //   // replace service id and update template later
-  //   // currently these messages will go to my email
-  //   // integrate netlify with sendForm code
-  //   emailjs
-  //     .sendForm("service_ovv4bhg", "template_mp16n74", form.current, {
-  //       publicKey: "dif7BQrUeE-vta83w",
-  //     })
-  //     .then(
-  //       (result) => {
-  //         setStateMessage("Message sent!");
-  //         setIsSubmitting(false);
-  //         setTimeout(() => {
-  //           setStateMessage(null);
-  //         }, 5000); //hides the message after 5 seconds
-  //       },
-  //       (error) => {
-  //         setStateMessage("Something went wrong, please try again later.");
-  //         setIsSubmitting(false);
-  //         setTimeout(() => {
-  //           setStateMessage(null);
-  //         }, 5000);
-  //       }
-  //     );
-  //   // Clears the form after sending the email
+  //   setStateMessage("Message sent!");
+  //   setTimeout(() => {
+  //     setStateMessage(null);
+  //     setIsSubmitting(false);
+  //   }, 5000);
   //   e.target.reset();
   // };
-
-  const handleSubmit = (e) => {
-    e.persist();
-    e.preventDefault();
-    setIsSubmitting(true);
-    setStateMessage("Message sent!");
-    setTimeout(() => {
-      setStateMessage(null);
-      setIsSubmitting(false);
-      e.target.reset();
-    }, 5000);
-  };
 
   return (
     <main className="contact-wrapper">
@@ -68,7 +69,7 @@ function Contact() {
       </div>
       <div className="right">
         {/* input name attributes must match the variables used in the emailjs template */}
-        <form name="contact" method="POST" onSubmit={handleSubmit}>
+        <form ref={form} name="contact" method="POST" onSubmit={sendEmail}>
           <input type="hidden" name="form-name" value="contact" />
           <label htmlFor="name">Name</label>
           <input
